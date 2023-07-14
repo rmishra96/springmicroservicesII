e80c1ce08329c17661208356d6281f3c6431d711
ranjanmishra96 <ranjan.mishra96@hotmail.com>
2023-07-14 19:47:38 +0530

    Axon Framework and Command Related Stuffs

--- Productservice/src/main/java/com/demo/example/product/Productservice/Rest/ProductController.java
+++ Productservice/src/main/java/com/demo/example/product/Productservice/Rest/ProductController.java
@@ -1,41 +1,60 @@
 package com.demo.example.product.Productservice.Rest;
 
+import java.util.UUID;
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.example.product.Productservice.command.CreateProductCommand;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-	
+
 	@Autowired
 	private Environment env;
+	private final CommandGateway commandGateway;
 
-    @PostMapping
-    public String createProduct(){
-        return "HTTP POST is Handled";
-    }
+	@Autowired
+	public ProductController(Environment env, CommandGateway commandGateway) {
+		this.env = env;
+		this.commandGateway = commandGateway;
+	}
 
+	@PostMapping
+	public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
 
-    @GetMapping
-    public String getProducts(){
-        return "HTTP Get request Handled";
-    }
+		CreateProductCommand createProductCommand = CreateProductCommand.builder()
+				.price(createProductRestModel.getPrice()).quantity(createProductRestModel.getQuantity())
+				.title(createProductRestModel.getTitle()).productId(UUID.randomUUID().toString()).build();
 
+		String returnValue;
 
-    @PutMapping
-    public String updateProduct(){
-        return "HTTP Put handled";
-    }
+		returnValue = commandGateway.sendAndWait(createProductCommand);
 
-    @DeleteMapping
-    public String deleteProduct(){
-        return "Http delete Handled";
-    }
+		return returnValue;
+	}
+
+	@GetMapping
+	public String getProducts() {
+		return "HTTP Get request Handled" + env.getProperty("local.server.port");
+	}
+
+	@PutMapping
+	public String updateProduct() {
+		return "HTTP Put handled";
+	}
+
+	@DeleteMapping
+	public String deleteProduct() {
+		return "Http delete Handled";
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # springmicroservicesII
+
+## Running Axon Server locally by mounting volumne
+docker run --name axonserver -p 8024:8024 -p 8124:8124 -v "/path\docker-data\data":/data 
+	-v "/path\docker-data\eventdata":/event-data -v "/path\docker-data\config":/config axoniq/axonserver
